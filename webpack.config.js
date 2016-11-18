e8891af2ea1a7ad30949109d7457819c91551d7c
caoyongzheng <15267056106@139.com>
2016-11-19 00:58:46 +0800

    init webpack project

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const production = process.env.NODE_ENV === 'production'
+
+const config = {
+  entry: {
+    vendor: ['vue', 'vue-router'],
+    app: './demo/index.js',
+  },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
+    path: path.resolve('./assets'),
+    publicPath: '/assets/',
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.(vue|js)$/,
+        exclude: [/node_modules/],
+        loader: 'eslint',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel',
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue',
+      },
+      {
+        test: /\.(css)$/,
+        exclude: [/node_modules/],
+        loader: 'style!css!postcss',
+      },
+    ],
+  },
+  resolve: {
+    root: [path.resolve('./src'), path.join('./node_modules')],
+    extensions: ['', '.js', '.vue'],
+    alias: {
+      src: path.resolve('./src'),
+      demo: path.resolve('./demo'),
+    },
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js',
+        minChunks: Infinity,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: production ? JSON.stringify('production') : JSON.stringify('development'),
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ],
+  postcss() {
+    return [require('autoprefixer'), require('precss')]
+  },
+  vue: {
+    loaders: { css: 'style!css!postcss' },
+    postcss() {
+      return [require('autoprefixer'), require('precss')]
+    },
+  },
+  devServer: {
+    contentBase: path.resolve('./demo'),
+    historyApiFallback: true,
+  },
+}
+
+module.exports = config
