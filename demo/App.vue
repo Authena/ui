e1e57ba828e8260e63312e103cf3183af9cbdb15
caoyongzheng <15267056106@139.com>
2016-11-19 18:03:37 +0800

    add LeftNav

--- demo/App.vue
+++ demo/App.vue
@@ -1,29 +1,29 @@
 <template lang="html">
   <div id="app">
-    <div class="paper paper-1">
+    <div class="left">
+      <LeftNav />
     </div>
-    <div class="paper paper-2">
-    </div>
-    <div class="paper paper-3">
-    </div>
-    <div class="paper paper-4">
-    </div>
-    <div class="paper paper-5">
+    <div class="right">
+      <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script>
 import '../src/paper.css'
+import router from './router'
+import LeftNav from './components/LeftNav'
 
 export default {
+  name: 'App',
   data() {
     return {}
   },
   computed: {},
   mounted() {},
   methods: {},
-  components: {},
+  components: { LeftNav },
+  router,
 }
 </script>
 
@@ -32,10 +32,26 @@ export default {
   margin: 0;
   padding: 0;
 }
+
+html, body{
+  width: 100%;
+  height: 100%;
+}
+
 </style>
 <style media="screen" scoped>
-.paper{
-  width: 100px;
-  height: 100px;
+#app{
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+  display: flex;
+}
+.left{
+  width: 256px;
+  height: 100%;
+}
+.right{
+  flex: 1;
+  overflow: scroll;
 }
 </style>
